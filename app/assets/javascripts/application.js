ab105e0978467705da8dbf5c06ad75d4d3c89347
Alan Donohoe <alandonohoe@hotmail.com>
2016-07-31 11:48:59 +0100

    add jquery modal and figaro and split out countdown and live pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,8 @@
+//= require jquery/dist/jquery
 //= require angular
 //= require angular-ui-router
-//= require jquery/dist/jquery
 //= require minimal_digital_countdown_plugin/dist/scripts/main
 //= require moment/min/moment.min
+//= require jquery-modal/jquery.modal
+//= require main
 //= require_tree .
-
-$(function(){
-  'use strict';
-  countDownTimer.init();
-});
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,5 @@
+// File that replicates is in the non-rails, gulp/bower only version
+$(function(){
+  'use strict';
+  countDownTimer.init();
+});
