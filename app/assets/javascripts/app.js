d3e9568007927f1247dedc923dfb7a9394a80217
Alan Donohoe <alandonohoe@hotmail.com>
2016-08-02 06:30:39 +0100

    post new emails to email-elephant

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,25 @@
 // Angular app.
 (function() {
   var app = angular.module('wars',[]);
-  app.controller('emailController', function() {
-    
+  app.controller('EmailController', function($http) {
+    this.emailAddress ='';
+    this.postEmail = function postEmail() {
+      console.log('EmailController emailAddress: ' + this.emailAddress);
+      postNewEmailAddress(this.emailAddress);
+      this.emailAddress ='';
+    };
+
+    function postNewEmailAddress(emailAddress) {
+      $http({
+        url: 'https://email-elephant.herokuapp.com/emails',
+        method: "POST",
+        data: JSON.stringify({email: {address: emailAddress, api_key: 'ea7cbe8fd41512f3'}}),
+        headers: {'Content-Type': 'application/json'}}
+      ).then(function successCallback(response) {
+        console.log('successCallback' + response);
+      }, function errorCallback(response) {
+        console.log('errorCallback' + response);
+      });
+    }
   });
 })();
