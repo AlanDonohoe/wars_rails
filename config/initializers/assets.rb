93ac07e70e7f4b3b62e077ce3ddb5d03d0ddf377
Alan Donohoe <alandonohoe@hotmail.com>
2016-09-11 19:56:14 +0100

    try diff config for img preprocessing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,3 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
