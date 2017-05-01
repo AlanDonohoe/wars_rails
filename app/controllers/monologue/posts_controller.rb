23af470257229d4f17b3813fccaa1159647f8d5f
Alan Donohoe <alandonohoe@hotmail.com>
2017-05-01 16:19:04 +0200

    fix method name call

--- app/controllers/monologue/posts_controller.rb
+++ app/controllers/monologue/posts_controller.rb
@@ -3,7 +3,7 @@ class Monologue::PostsController < Monologue::ApplicationController
     @page = params[:page].nil? ? 1 : params[:page]
     top_tag = Monologue::Tag.where(name: 'always-at-top').first
     @top_post = taggings.first.post if top_tag.present
-    if @top_post.present
+    if @top_post.present?
       @posts = Monologue::Post.page(@page).includes(:user).where.not(id: @top_post.id).published
       @posts = [@top_post] + @posts.to_a
     else
