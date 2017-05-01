54c6725e68571712df32f5a73afa2cdef082e77d
Alan Donohoe <alandonohoe@hotmail.com>
2017-05-01 16:13:53 +0200

    deal with no top posts

--- app/controllers/monologue/posts_controller.rb
+++ app/controllers/monologue/posts_controller.rb
@@ -1,20 +1,12 @@
 class Monologue::PostsController < Monologue::ApplicationController
   def index
     @page = params[:page].nil? ? 1 : params[:page]
-    @top_post = Monologue::Tag.where(name: 'always-at-top').first.taggings.first.post
     @posts = Monologue::Post.page(@page).includes(:user).where.not(id: @top_post.id).published
-    puts '@posts ' + @posts.count.inspect
-    @posts.to_a + [@top_post]
-    puts '@posts ' + @posts.count.inspect
-    # top_tag = Monologue::Tag.where(name: 'always-at-top').first
-    # top_post = top_tag.taggings.first.post
-    # puts '@posts.count ' + @posts.count.inspect
-    # @posts.to_a - [top_post] if top_post.present?
-    # puts 'top_tag ' + top_tag.inspect
-    # puts 'top_post ' + top_post.inspect
-    # puts '@posts.count ' + @posts.count.inspect
-    # top_post = @posts.includes(:tags).where(tags: { name: 'always-at-top' })
+    top_tag = Monologue::Tag.where(name: 'always-at-top').first
+    @top_post = taggings.first.post if top_tag.present
+    @posts = [@top_post] + @posts.to_a if @top_post.present
   end
+
   def show
     if monologue_current_user
       @post = Monologue::Post.default.where("url = :url", {url: params[:post_url]}).first
